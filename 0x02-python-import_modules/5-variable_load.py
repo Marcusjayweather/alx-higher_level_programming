a4c618058b274d1bbfd58ada8ddfa79c7786da97
marcusjayweather <marcusjnr61@gmail.com>
2023-10-05 09:03:09 -0700

    marcusjunior

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 # 5-variable_load.py
+from variable_load_5 import a
 
-if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
 
+def find_var():
     print(a)
 
+
+if __name__ == "__main__":
+    find_var()
