2b76e7ca75f809cf4b8f39a2f14b0c0059c16015
marcusjayweather <marcusjnr61@gmail.com>
2023-10-05 09:01:19 -0700

    marcusjunior

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 # 4-hidden_discovery.py
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
     import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
+    # Print sorted name from directory
+    for name in sorted(dir(hidden_4)):
+        # print only names that do not start with __
+        if name[:2] != '__':
+            print("{}".format(name))
