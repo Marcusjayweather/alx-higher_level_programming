0c282214c5c96f5d7a63ba25477b9fcd46eabc3f
marcusjayweather <marcusjnr61@gmail.com>
2024-01-26 03:26:50 -0800

    network1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
 """
-Please list 10 commits (from the most recent to oldest) of the repository
-“rails” by the user “rails”
-You must use the GitHub API, here is the documentation
-https://developer.github.com/v3/repos/commits/
-Print all commits by: `<sha>: <author name>` (one by line)
+Takes your GitHub credentials (username and password) and uses the GitHub API
+to display your id
 """
-
 
 if __name__ == '__main__':
     from requests import get
     from sys import argv
 
-    repo = argv[1]
-    owner = argv[2]
-    i = 0
+    username = argv[1]
+    password = argv[2]
 
-    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
-
-    response = get(URL)
+    URL = "https://api.github.com/user"
+    response = get(URL, auth=(username, password))
     json = response.json()
 
-    for element in json:
-        if i > 9:
-            break
-        sha = element.get('sha')
-        author = element.get('commit').get('author').get('name')
-        print("{}: {}".format(sha, author))
-        i += 1
+    print(json.get('id'))
+
